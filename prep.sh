1cd08153939949e7301aa9fe12b80c2303f2737e
MTijbout <marco.tijbout@gmail.com>
2019-12-24 00:05:52 +0100

    up

--- prep.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-timedatectl set-ntp true
-rm ArchInstallation.sh
-rm diskSectorCalculator.sh
-touch ArchInstallation.sh
-touch diskSectorCalculator.sh
-chmod +x *.sh
-nano ArchInstallation.sh
